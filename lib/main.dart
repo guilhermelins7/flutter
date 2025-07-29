c0d1f9591868fc882b28176c2b8dd879151d4b03
Guilherme Lins <guilherme.santos@modalgr.io>
2025-07-29 09:19:25 -0300

    feat: primeiro widget task

--- lib/main.dart
+++ lib/main.dart
@@ -20,30 +20,47 @@ class MyApp extends StatelessWidget {
           title: Text('Tarefas', style: TextStyle(color: Colors.white)),
           backgroundColor: Color(0xFF2F80ED),
         ),
-        body: Container(
-          color: Colors.white,
-          child: Stack(
-            children: [
-              Container(color: Colors.blue, height: 140),
-              Container(
-                color: Colors.white70,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(color: Colors.black26, width: 72, height: 100),
-                    Text('Aprender Flutter'),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Icon(Icons.arrow_drop_up),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+        body: Column(
+          children: [
+            Task('Aprender Flutter'),
+            Task('Andar de bike'),
+            Task('Voar'),
+          ],
         ),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    );
+  }
+}
+
+class Task extends StatelessWidget {
+  final String nome; // Imutável após ser definida, comum em StatelessWidget.
+
+  const Task(this.nome, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Stack(
+        children: [
+          Container(color: Colors.blue, height: 140),
+          Container(
+            color: Colors.white70,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(color: Colors.black26, width: 72, height: 100),
+                Text(nome),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Icon(Icons.arrow_drop_up),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
