bccc93f1f953a8d0aebb386a1756f6713fe8817a
guilhermelins7 <guilherme7lins@gmail.com>
2025-08-31 18:11:38 -0300

    feat: Exibição padrão quando não há imagem selecionada, tipos de input e scroll do formulario

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -18,91 +18,116 @@ class _FormScreenState extends State<FormScreen> {
     return Scaffold(
       appBar: AppBar(title: Text('Nova Tarefa')),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 0, right: 8, bottom: 16, left: 8),
-          child: Container(
-            height: 675,
-            width: 355,
-            decoration: BoxDecoration(
-              color: Colors.black12,
-              border: Border.all(width: 3),
-              borderRadius: BorderRadius.circular(10),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 0,
+              right: 8,
+              bottom: 16,
+              left: 8,
             ),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    controller: nameController,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Nome', // definindo o placeholder
-                      fillColor: Colors.white70,
-                      filled: true,
+            child: Container(
+              height: 675,
+              width: 355,
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                border: Border.all(width: 3),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.text,
+                      controller: nameController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Nome', // definindo o placeholder
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    controller: dificultyController,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Dificuldade', // definindo o placeholder
-                      fillColor: Colors.white70,
-                      filled: true,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: dificultyController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Dificuldade', // definindo o placeholder
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    onChanged: (text) {
-                      setState(
-                        () {},
-                      ); // Atualizar imagem sempre que mudar o campo.
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.url,
+                      onChanged: (text) {
+                        setState(
+                          () {},
+                        ); // Atualizar imagem sempre que mudar o campo.
+                      },
+                      controller: imageController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Imagem', // definindo o placeholder
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 72,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(width: 2, color: Colors.blue),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        imageController.text,
+                        errorBuilder:
+                            (
+                              BuildContext context,
+                              Object exception,
+                              StackTrace? stackTrace,
+                            ) {
+                              return Image.asset(
+                                'assets/images/nophoto.png',
+                                fit: BoxFit.cover,
+                              );
+                            },
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print(
+                        'Conteúdo do contoller nome: ${nameController.text}',
+                      );
+                      print(
+                        'Conteúdo do contoller dificuldade: ${dificultyController.text}',
+                      );
+                      print(
+                        'Conteúdo do contoller imagem: ${imageController.text}',
+                      );
                     },
-                    controller: imageController,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Imagem', // definindo o placeholder
-                      fillColor: Colors.white70,
-                      filled: true,
-                    ),
+                    child: Text('Adicionar'),
                   ),
-                ),
-                Container(
-                  height: 100,
-                  width: 72,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(width: 2, color: Colors.blue),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.network(
-                      imageController.text,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    print('Conteúdo do contoller nome: ${nameController.text}');
-                    print(
-                      'Conteúdo do contoller dificuldade: ${dificultyController.text}',
-                    );
-                    print(
-                      'Conteúdo do contoller imagem: ${imageController.text}',
-                    );
-                  },
-                  child: Text('Adicionar'),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
